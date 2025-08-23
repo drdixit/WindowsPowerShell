68cc0827d16c4bea109f4660cb36f8aadfea2c64
Dixit <drdixit6@gmail.com>
2025-08-23 16:30:17 +0530

    basic

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+﻿function whereis ($command) {
+    Get-Command -Name $command -All -ErrorAction SilentlyContinue |
+    Select-Object Name, CommandType, Path |
+    Format-Table -AutoSize
+}
+
+function prompt { "$([char]27)[36m$(Get-Location) $([char]27)[38;5;220m✘$([char]27)[0m " }
